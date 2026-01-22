a9a2ce31877310c23466f50ab4a72658fb00172b
stanleywalkercoatzadrone <stanley.walker@coatzadroneusa.com>
2026-01-22 15:10:57 -0500

    fix: Standardize CSS build pipeline

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // Global Styles
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
 
